90b379f20ed25310f734794e38fb1d91bfe5c862
Resume <59206519+tanya4067@users.noreply.github.com>
2026-08-05 02:06:57 +0530

    Add: combination-target-sum-ii - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def temp(self,ans,result,candidates,n,target,i,dp):
+        if(target<0):
+            return
+        
+        if(target==0):
+            result.append(ans.copy())
+            return
+        for j in range(i,n):
+            if j > i and candidates[j] == candidates[j-1]:
+                continue
+            ans.append(candidates[j])
+            self.temp(ans,result,candidates,n,target-candidates[j],j+1,dp)
+            ans.pop()
+
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans=[]
+        result=[]
+        n=len(candidates)
+        candidates=sorted(candidates)
+        dp=[]
+        self.temp(ans,result,candidates,n,target,0,dp)
+
+        return(result)
+        
